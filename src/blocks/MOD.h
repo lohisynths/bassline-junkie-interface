1ce936bce54dbadec94417fbaf99fbd9599f7f73
lohisynths <lohisynths@gmail.com>
2018-09-25 20:06:53 +0200

    mod cleanup

--- src/blocks/MOD.h
+++ src/blocks/MOD.h
@@ -26,86 +26,16 @@
 // OSC2 pitch, OSC2 sin, OSC2 saw, OSC2 sqr, OSC2 rnd          = 5
 // FLT freq, FLT reso                                          = 2
 //                                                               17
-#define  MOD_DST_COUNT 17
+#define  MOD_PARAM_COUNT 17
 
-#define MOD_SRC_COUNT               (6)
-
-enum MOD_PARAMS {
-    MOD_AMT,
-    MOD_SRC,
-    MOD_PARAM_COUNT
-};
 
 class MOD : public UI_BLOCK<MOD_KNOB_COUNT, MOD_BUTTON_COUNT, MOD_PARAM_COUNT, MOD_COUNT> {
 public:
-    typedef std::array<int, MOD_DST_COUNT> dst_preset;
-    typedef std::array<dst_preset, MOD_COUNT> preset;
-
-    preset &get_preset() {
-        return preset_values;
-    }
-
-    void dump_midi() {
-        LOG::LOG0("%s UI_BLOCK dump midi\r\n", get_name());
-        for (int j = 0; j < MOD_DST_COUNT; j++) {
-            for (int i = 0; i < MOD_COUNT; i++) {
-                int value = preset_values[i][j];
-                //int index = (i*MOD_DST_COUNT) + j;
-                uint8_t midi_nr = get_midi_nr(i, j);
-                midi->send_cc(midi_nr, value, get_midi_ch());
-            }
-        }
-    }
-
-    void set_preset(preset input) {
-        memcpy(&preset_values, &input, sizeof(input));
-        select_instance(current_instance);
-        dump_midi();
-    };
 
     /*! \typedef logger
      *  \brief Typedef defining logger used in all instances of this object
      */
     typedef logger<MOD_LOG_LEVEL> LOG;
-
-	~MOD(){};
-
-	virtual const char* get_name() {
-	    return "MOD";
-	}
-
-    virtual int16_t get_current_preset_value(uint8_t index) {
-        return preset_values[current_instance][actual_mod_dest];
-    }
-
-    virtual void set_current_preset_value(uint8_t index, uint16_t value) {
-        preset_values[current_instance][actual_mod_dest] = value;
-    }
-
-    void select_mod_src(uint8_t index, bool force = 0) {
-        if(current_instance != index || force) {
-            LOG::LOG0("%s src %d selected\r\n", get_name(), index);
-            turn_off_sw(current_instance);
-            turn_on_sw(index);
-            set_current_preset_value(MOD_SRC, index);
-            current_instance = index;
-        }
-    }
-
-    void select_MOD_dest(int index) {
-        LOG::LOG0("%s dst %d selected\r\n", get_name(), index);
-        actual_mod_dest = index;
-    }
-
-    void select_mode(uint8_t index) {
-        LOG::LOG0("%s select_mode %d\r\n", get_name(), index);
-    }
-
-    void force_mode(uint8_t index) {
-        // ignore index
-        LOG::LOG0("%s force_mode %d\r\n", get_name(), current_instance);
-        select_MOD_dest(actual_mod_dest);
-    }
 
 	void init(Mux *mux, Pwm *leds, MIDI *midi_) {
 		midi = midi_;
@@ -115,7 +45,8 @@ public:
 		uint16_t button_max_led_val = BUTTON_MAX_LED_VAL;
 
 		knob_config knob_ctrl={
-			Knob::knob_init_map{mux, mux->get(2), 0, knob_max_val, leds, knob_led_max_val, (MOD_FIRST_ENC_LED +  0), knob_led_count},
+			Knob::knob_init_map{mux, mux->get(2), 0, knob_max_val,
+			                    leds, knob_led_max_val, (MOD_FIRST_ENC_LED +  0), knob_led_count},
 		};
 
 		button_config button_ctrl={
@@ -130,25 +61,43 @@ public:
 		select_instance(current_instance);
 	}
 
-	virtual void knob_sw_changed(uint8_t index, bool state) {
+    virtual ~MOD(){};
 
-	}
+    virtual const char* get_name() {
+        return "MOD";
+    }
 
-    uint8_t get_midi_ch(){
+    virtual int16_t get_current_preset_value(uint8_t index) {
+        return preset_values[current_instance][actual_mod_dest];
+    }
+
+    virtual void set_current_preset_value(uint8_t index, uint16_t value) {
+        preset_values[current_instance][actual_mod_dest] = value;
+    }
+
+    void select_MOD_dest(int index) {
+        LOG::LOG0("%s dst %d selected\r\n", get_name(), index);
+        actual_mod_dest = index;
+    }
+
+    virtual void select_mode(uint8_t index) { }
+    virtual void force_mode(uint8_t index) { }
+    virtual void knob_sw_changed(uint8_t index, bool state) { }
+
+    virtual uint8_t get_midi_ch(){
         return 2;
     }
 
-    uint8_t get_midi_nr(uint8_t instance, uint8_t index) {
-        return instance + (index*MOD_SRC_COUNT);
+    virtual uint8_t get_midi_nr(uint8_t instance, uint8_t index) {
+        return instance + (index*MOD_COUNT);
     }
 
-    uint8_t get_current_instance_midi_nr(uint8_t index) {
+    virtual uint8_t get_current_instance_midi_nr(uint8_t index) {
         return get_midi_nr(current_instance, index);
     }
 
 private:
 	uint8_t actual_mod_dest = 0;
-    preset preset_values = {};
 };
 
 #endif /* SRC_MOD_H_ */
