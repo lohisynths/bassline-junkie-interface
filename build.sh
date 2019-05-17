116c3e5a7f1f57c7c9880564332f5a6da0a29e87
lohisynths <lohisynths@gmail.com>
2019-05-17 17:12:25 +0200

    small changes in buildsystem

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mkdir BUILD
 rm -rf BUILD/*
 cd BUILD
 # -DCMAKE_VERBOSE_MAKEFILE=ON
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/nucleo_l432kc_gcc.cmake -DMBED_DIR=/home/alax/git/nucleo_mbed/mbed-os/BUILD/mbed -DCMAKE_BUILD_TYPE=Debug
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/nucleo_l432kc_gcc.cmake -DMBED_DIR=/home/alax/git/mbed-os/BUILD/mbed -DCMAKE_BUILD_TYPE=Debug -DCMAKE_VERBOSE_MAKEFILE=ON
 make -j8
 cd ../
 
